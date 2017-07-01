3c297a1e240455bb6a3ff670af94121bdbd2a99d
Philip O'Toole <philip.otoole@yahoo.com>
2017-07-01 11:42:41 -0700

    Start moving towards proper gRPC service

--- service/service.go
+++ service/service.go
@@ -2,14 +2,47 @@ package service
 
 import (
 	"database/sql"
+	"log"
+	"net"
+	"os"
+
+	pb "github.com/otoolep/go-grpc-pg/proto"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 type Service struct {
+	g  *grpc.Server
 	db *sql.DB
+
+	ln   net.Listener
+	addr net.Addr
+
+	logger *log.Logger
 }
 
-func NewService(db *sql.DB) *Service {
-	return &Service{
-		db: db,
+func NewService(ln net.Listener, db *sql.DB) *Service {
+	s := Service{
+		g:      grpc.NewServer(),
+		db:     db,
+		ln:     ln,
+		addr:   ln.Addr(),
+		logger: log.New(os.Stderr, "[service] ", log.LstdFlags),
 	}
+
+	pb.RegisterDBProviderServer(s.g, &s)
+	return &s
+}
+
+func (s *Service) Addr() string {
+	return s.addr.String()
+}
+
+func (s *Service) Query(context.Context, *pb.QueryRequest) (*pb.QueryResponse, error) {
+	return nil, nil
+}
+
+func (s *Service) Exec(context.Context, *pb.ExecRequest) (*pb.ExecResponse, error) {
+	return nil, nil
 }
