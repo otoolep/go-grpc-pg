7288fd592114929d33d7ce3d30ed6066dbe4f62e
Philip O'Toole <philip.otoole@yahoo.com>
2017-07-03 21:31:13 -0700

    Use a package name

--- doc.go
+++ doc.go
@@ -1,3 +1,5 @@
 /*
-Package go-grpc-pg is a simple example of a service that exposes a gRPC interface, and contacts a PostgreSQL backend.
+Package gogrpcpg is a simple example of a service that exposes a gRPC interface, and contacts a PostgreSQL backend.
 */
+
+package gogrpcpg
